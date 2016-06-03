3c8c2da1b734c985894f60a9a18e7098fadcd4d0
razdobreeva <razbober@gmail.com>
2016-06-04 05:58:17 +0600

    Личинка больше не личинка
    Написаны функции вычисления количества шагов и расстояния, добавлены
    распечатки значений вместо названий. Но рамки все равно красные. :(

--- build/js/check.js
+++ build/js/check.js
@@ -1,5 +1,23 @@
 'use strict';
 
+function calculateSteps (a) {
+  var sum = 0;
+  for (var i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+  return sum;
+}
+
+function calculateDistance (a, b) {
+  var distance = 0;
+  for (var i = 0; i < a.length; i++) {
+    for (var j = 0; j < b.length; j++) {
+      distance += a[i]*b[j];
+    }
+  }
+  return distance;
+}
+
 function getMessage(a, b) {
   if (typeof a === 'boolean') {
     if (a) {
@@ -10,17 +28,17 @@ function getMessage(a, b) {
     }
   }
   else if (typeof a === 'number') {
-    return 'Я прыгнул на [a]*100 см';
+    return 'Я прыгнул на ' + a*100 + ' см';
   }
   else if (typeof a === 'object' && typeof b === 'object') {
-    return 'Я прошел [length] метров';
+    return 'Я прошел ' + calculateDistance (a, b) + ' метров';
     //[length] - сумма произведений
     //соответствующих элементов массивов a и b,
     //cумма произведения первого элемента a с первым элементом b,
     //второго со вторым и так далее
   }
   else if (typeof a === 'object') {
-    return 'Я прошел [sum] шагов';
+    return 'Я прошел ' + calculateSteps(a) + ' шагов';
     //[sum] - сумма значений переданного массива
   }
 }
