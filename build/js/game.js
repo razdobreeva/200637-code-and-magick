352409a99dc1dd9c684e78029c10652f5f8c1b55
razdobreeva <razbober@gmail.com>
2016-06-24 00:13:23 +0600

    ADD
    Добавлена примерная отрисовка сообщения в случае выигрыша

--- build/js/game.js
+++ build/js/game.js
@@ -379,9 +379,44 @@
      */
     _drawPauseScreen: function() {
       switch (this.state.currentStatus) {
-        case Verdict.WIN:
+        case Verdict.WIN: {
+          //this.canvas = document.createElement('canvas');
+          //this.canvas = document.querySelector('canvas'); // DOM-элемент канваса
+          this.ctx = this.canvas.getContext('2d'); // контекст отрисовки
+          this.ctx.fillStyle = '#FFFFFF';
+          this.ctx.beginPath();
+          this.ctx.moveTo(225, 20);
+          this.ctx.lineTo(475, 20);
+          this.ctx.lineTo(475, 140);
+          this.ctx.lineTo(210, 155);
+          this.ctx.lineTo(225, 20);
+
+          //ObjectType.ME.x = 1;
+
+          // WIDTH / 3,
+          //y: HEIGHT - 100
+
+          //this.ctx.moveTo(WIDTH / 3 + 165, HEIGHT - 280);
+          //this.ctx.lineTo(430, 20);
+          //this.ctx.lineTo(430, 140);
+          //this.ctx.lineTo(185, 155);
+          //this.ctx.lineTo(200, 20);
+
+          this.ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+          this.ctx.shadowOffsetX = 10;
+          this.ctx.shadowOffsetY = 10;
+          this.ctx.fill();
+
+          this.ctx.font = '16px PT Mono';
+          this.ctx.textColor = 'black';
+          this.ctx.textBaseline = 'hanging';
+          this.ctx.fillText('Вы выиграли о_О', 225, 40);
+          this.ctx.fillText('Принимайте поздравления!', 225, 60);
+
           console.log('you have won!');
           break;
+        }
+
         case Verdict.FAIL:
           console.log('you have failed!');
           break;
