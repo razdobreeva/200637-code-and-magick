51799327e5f3cbb8963386b9f4b6ff1e87ccf125
razdobreeva <razbober@gmail.com>
2016-07-01 23:35:52 +0600

    FIX
    Рефакторинг всего. Новая универсальная функция на все события.

--- src/form.js
+++ src/form.js
@@ -5,21 +5,14 @@
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
 
+  var formName = document.querySelector('#review-name');
+  var formText = document.querySelector('#review-text');
   var marks = document.querySelector('.review-form-group-mark');
-  var mark1 = document.querySelector('#review-mark-1');
-  var mark2 = document.querySelector('#review-mark-2');
-  var name = document.querySelector('#review-name');
-  var text = document.querySelector('#review-text');
-  var reviewFields = document.querySelector('.review-fields');
-  var reviewFieldName = document.querySelector('.review-fields-name');
-  var reviewFieldText = document.querySelector('.review-fields-text');
-  var submitButton = document.querySelector('.review-submit');
 
   formOpenButton.onclick = function(evt) {
     evt.preventDefault();
     formContainer.classList.remove('invisible');
-    submitButton.disabled = true;
-    reviewFieldText.classList.add('invisible');
+    validateForm();
   };
 
   formCloseButton.onclick = function(evt) {
@@ -27,55 +20,49 @@
     formContainer.classList.add('invisible');
   };
 
-  name.oninput = function() {
-    if (name.value !== '') {
-      reviewFieldName.classList.add('invisible');
-      if (reviewFieldText.classList.contains('invisible')) {
-        reviewFields.classList.add('invisible');
-        submitButton.disabled = false;
-      }
-    } else { // случай, когда пользователь набрал логин, но потом стер его
-      reviewFields.classList.remove('invisible');
-      reviewFieldName.classList.remove('invisible');
-      submitButton.disabled = true;
-    }
+  formName.oninput = function() {
+    validateForm();
   };
 
-  text.oninput = function() {
-    if (text.value !== '') {
-      reviewFieldText.classList.add('invisible');
-      if (reviewFieldName.classList.contains('invisible')) {
-        reviewFields.classList.add('invisible');
-        submitButton.disabled = false;
-      }
-    } else {
-      if (mark1.checked || mark2.checked) {
-        reviewFields.classList.remove('invisible');
-        reviewFieldText.classList.remove('invisible');
-        submitButton.disabled = true;
-      } else {
-        if (name.value !== '') {
-          reviewFields.classList.add('invisible');
-          submitButton.disabled = true;
-        }
-        reviewFieldText.classList.add('invisible');
-      }
-    }
+  formText.oninput = function() {
+    validateForm();
   };
 
   marks.onchange = function() {
-    if (mark1.checked || mark2.checked) {
-      if (text.value === '') {
-        reviewFields.classList.remove('invisible');
-        reviewFieldText.classList.remove('invisible');
-        submitButton.disabled = true;
-      }
-    } else {
-      reviewFieldText.classList.add('invisible');
-      if (name.value !== '') {
-        reviewFields.classList.add('invisible');
-        submitButton.disabled = false;
-      }
-    }
+    validateForm();
   };
 })();
+
+function validateForm() {
+  var checkedMark = document.querySelector('input[name="review-mark"]:checked');
+  var formName = document.querySelector('#review-name');
+  var formText = document.querySelector('#review-text');
+  var reviewFields = document.querySelector('.review-fields');
+  var reviewFieldName = document.querySelector('.review-fields-name');
+  var reviewFieldText = document.querySelector('.review-fields-text');
+  var submitButton = document.querySelector('.review-submit');
+
+  if (formName.value !== '') {
+    reviewFieldName.classList.add('invisible');
+  } else { // случай, когда пользователь набрал логин, но потом стер его
+    reviewFieldName.classList.remove('invisible');
+  }
+
+  if (parseInt(checkedMark.value, 10) < 3) {
+    if (formText.value !== '') {
+      reviewFieldText.classList.add('invisible');
+    } else {
+      reviewFieldText.classList.remove('invisible');
+    }
+  } else {
+    reviewFieldText.classList.add('invisible');
+  }
+
+  if (reviewFieldName.classList.contains('invisible') && reviewFieldText.classList.contains('invisible')) {
+    reviewFields.classList.add('invisible');
+    submitButton.disabled = false;
+  } else {
+    reviewFields.classList.remove('invisible');
+    submitButton.disabled = true;
+  }
+}
