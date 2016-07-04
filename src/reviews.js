c3463b45815c40ba0c5e4d5017b45a54776a0864
razdobreeva <razbober@gmail.com>
2016-07-04 16:08:52 +0600

    ADD
    Добавление и удаление класса invisible у блока reviews-filter.
    Переименование переменных

--- src/reviews.js
+++ src/reviews.js
@@ -1,21 +1,11 @@
 'use strict';
 
-/*<template id="review-template">
-  <article class="review">
-    <img src="" class="review-author" alt="" title="">
-    <span class="review-rating"></span>
-    <p class="review-text"></p>
-    <div class="review-quiz">
-      Полезный отзыв?
-      <span class="review-quiz-answer review-quiz-answer-yes">Да</span>
-      <span class="review-quiz-answer review-quiz-answer-no">Нет</span>
-    </div>
-  </article>
-</template>*/
-
+var reviewsFilter = document.querySelector('.reviews-filter');
 var reviewsContainer = document.querySelector('.reviews-list');
 var templateElement = document.querySelector('template');
 var elementToClone;
+
+reviewsFilter.classList.add('invisible');
 
 if ('content' in templateElement) {
   elementToClone = templateElement.content.querySelector('.review');
@@ -29,31 +19,31 @@ var getReviewElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
   console.log(data);
 
-  element.querySelector('.review-author').textContent = data.author;
+  element.querySelector('.review-author').textContent = data.author.name;
   element.querySelector('.review-rating').textContent = data.rating;
   element.querySelector('.review-text').textContent = data.description;
   element.querySelector('.review-quiz').textContent = data.review_usefulness;
 
   container.appendChild(element);
 
-  var backgroundImage = new Image();
-  var backgroundLoadTimeout;
+  var avatar = new Image();
+  var avatarLoadTimeout;
 
-  backgroundImage.onload = function(evt) {
-    clearTimeout(backgroundLoadTimeout);
-    this.width = 124;
-    this.height = 124;
+  avatar.onload = function(evt) {
+    clearTimeout(avatarLoadTimeout);
+    avatar.style.width = 124;
+    avatar.style.height = 124;
     element.style.backgroundImage = 'url(\'' + evt.target.src + '\')';
   };
 
-  backgroundImage.onerror = function() {
+  avatar.onerror = function() {
     element.classList.add('review-load-failure');
   };
 
-  backgroundImage.src = data.author.picture;
+  avatar.src = data.author.picture;
 
-  backgroundLoadTimeout = setTimeout(function() {
-    backgroundImage.src = '';
+  avatarLoadTimeout = setTimeout(function() {
+    avatar.src = '';
     element.classList.add('review-load-failure');
   }, IMAGE_LOAD_TIMEOUT);
 
@@ -63,3 +53,5 @@ var getReviewElement = function(data, container) {
 window.reviews.forEach(function(review) {
   getReviewElement(review, reviewsContainer);
 });
+
+reviewsFilter.classList.remove('invisible');
